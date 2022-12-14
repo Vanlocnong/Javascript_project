0ddbca7bb11666834f9e845add0e3e4bfb7ed2d1
Vanlocnong <95829802+Vanlocnong@users.noreply.github.com>
2022-12-14 12:53:30 +0700

    14/12 project1_Tab_UI
    PROJECT TAB UI

---
+++ project1_tab_ui/main.js
@@ -0,0 +1,30 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const selectTabsItems = $$('.tab-item');
+const selectTabsPanes = $$('.tab-pane');
+
+const line = $('.line')
+const tabActive = $('.tabs')
+console.log([tabActive])
+
+line.style.left = tabActive.offsetLeft + 'px'
+line.style.width = tabActive.offsetWidth + 'px'
+
+
+
+selectTabsItems.forEach((curr , index) => {
+
+        const paneContent = selectTabsPanes[index]
+
+        curr.onclick = function(event){
+        $('.tab-item.active').classList.remove('active')
+        curr.classList.add('active')
+        $('.tab-pane.active').classList.remove('active')
+        paneContent.classList.add('active')
+
+        line.style.left = this.offsetLeft + 'px'
+        line.style.width = this.offsetWidth + 'px'
+
+    }
+})
