da09c47569ba214fe459dc2790791ec2c940ebe2
Vanlocnong <95829802+Vanlocnong@users.noreply.github.com>
2023-03-21 22:15:37 +0700

    21/03
    add func check password , confirmation password

--- project_validation_form/validator.js
+++ project_validation_form/validator.js
@@ -1,65 +1,84 @@
-// Validator object
-function validator(object){
+function validator (object){
 
-    //ham thuc hien validate
-    function validate(inputElement,rule){
-        var errorMassage = rule.test(inputElement.value)
-        var errorElement = inputElement.parentElement.querySelector('.form-message')
-        if(errorMassage){
-            errorElement.innerHTML = errorMassage;
-            inputElement.parentElement.classList.add('invalid');
-            
-        }else{
-            errorElement.innerHTML = "";
+
+    //tao phuong thuc validat de kiem thuc hien kiem tra va cap nhat HTML
+    //dua vao value cua inputElement & ket qua tra tu pthuc test cua doi tuong 
+
+    function validate(inputElement , rule){
+        var formMessage = inputElement.parentElement.querySelector('.form-message')
+        var errorMessage = rule.test(inputElement.value)
+        if(errorMessage){
+            formMessage.innerHTML = errorMessage
+            inputElement.parentElement.classList.add('invalid')
+        }else {
+            formMessage.innerHTML = ""
             inputElement.parentElement.classList.remove('invalid')
         }
     }
-    
-    function removeError(){
-        
+    function clearErrorMessage (inputElement){
+        var formMessage = inputElement.parentElement.querySelector('.form-message')
+        formMessage.innerHTML = ""
+        inputElement.parentElement.classList.remove("invalid")
     }
 
-    //1.lay element cua form can validate
-    var formElement = document.querySelector(object.form)
-    if(formElement){
-        object.rules.forEach (function(rule){
-            var inputElement =  formElement.querySelector(rule.selector)
-            if (inputElement) {
-                inputElement.onblur = function (){
-                    //value : inputElement.value
-                    //test func  : \rule.test
-                    validate(inputElement, rule)
+    //lay ra form can validate
+    let formElement = document.querySelector(object.form)
+    if(formElement) {
+        object.rules.forEach(function(rule){
+            var inputElement = formElement.querySelector(rule.selector)
+            if(inputElement) {
+                inputElement.onblur = function() {
+                    validate(inputElement,rule)
                 }
+                inputElement.oninput = function () {
+                    clearErrorMessage(inputElement)
+                }
+                
             }
+            
         })
     }
 }
 
 
 
-
-
-
-//dinh nghia cac rules
-//1.khi co loi  tra ra message loi
-//2.khi hop le => undefine
 validator.isRequired = function(selector){
     return {
         selector : selector,
         test : function (value){
-            return value.trim() ? undefined : "Vui long nhap truong nay"
+            return value.trim() ? undefined : "Vui long nhap fied nay"
         }
     }
 }
 
-validator.isEmail = function (selector){
+validator.isEmail = function(selector){
     return {
-        selector: selector,
-        test : function (value) {
-            var emailValidation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-            return emailValidation.test(value) ? undefined : "Field nay phai la Email"
+        selector : selector,
+        test : function (value){
+            var checkEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+            return checkEmail.test(value) ? undefined : "Fied nay phai la Email"
         }
-        
     }
 }
 
+validator.checkPassword = function (selector , min) {
+    return {
+        selector : selector,
+        test : function (value){
+            if (value.length >= min){
+                return undefined
+            }else {
+                return "Mat khau khoi tao tu 7 ky tu tro len"
+            }
+        }
+    }
+}
+
+validator.confirmPassword = function (selector , getConfirmValue) {
+    return {
+        selector : selector,
+        test : function (value){
+            return value === getConfirmValue() ? undefined : "Mat khau xac nhan khong trung khop"
+        }
+    }
+}
